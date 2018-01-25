eb00d4c5d684d6f050a200b28e84147ca9d7d1a4
aureole420 <tongyuhui2009@gmail.com>
2018-01-24 19:43:43 -0600

    tested eureka and hystrix

--- order-payment-service/src/main/java/demo/Service/impl/DefaultDeliveryTimeService.java
+++ order-payment-service/src/main/java/demo/Service/impl/DefaultDeliveryTimeService.java
@@ -4,7 +4,11 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import demo.Service.DeliveryTimeService;
 import demo.domain.PaymentInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,14 +19,29 @@ public class DefaultDeliveryTimeService implements DeliveryTimeService {
     @Value("${com.yuhui.cs504.order.distribution.service}")
     private String orderDistribution;
 
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
     private RestTemplate restTemplate = new RestTemplate();
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
 
     @HystrixCommand(fallbackMethod = "estimateDeliveryTimeFallBack")
     @Override
     public void estimateDeliveryTime(PaymentInfo paymentInfo) {
-        log.info("The order-payment-service is calling the REST API of order-distribution-service");
-        restTemplate.postForLocation(orderDistribution+"/mq/orders", paymentInfo);
+
+//            discoveryClient.getInstances("order-distribution-service");
+//            String orderDistribution = "http://order-distribution-service";
+            log.info("The order-payment-service is calling the REST API of order-distribution-service");
+            log.info("<http://order-distribution-service> is " + orderDistribution);
+            this.restTemplate.postForLocation(orderDistribution + "/mq/orders", paymentInfo);
+
     }
 
     public void estimateDeliveryTimeFallBack(PaymentInfo paymentInfo) {
